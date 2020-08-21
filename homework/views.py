589ddbcf2929b5065abd53a347baf2f964af7378
Jawqpuizz <45869766+Jawqpuizz@users.noreply.github.com>
2020-08-21 19:02:57 -0400

    updated

--- homework/views.py
+++ homework/views.py
@@ -13,12 +13,12 @@ def signup(request):
         if request.POST.get('userrole') and request.POST.get('username') and request.POST.get(
                 'email') and request.POST.get('password'):
             save_record = InsertNewUser()
-            save_record.userrole = request.POST.get('userrole')
+            save_record.user_role = request.POST.get('userrole')
             save_record.username = request.POST.get('username')
             save_record.email = request.POST.get('email')
             save_record.password = request.POST.get('password')
             save_record.save()
-            messages.success(request, 'You are successfully registered')
+
             if request.POST.get('userrole') == 'Student':
                 return render(request, 'students.html',{'username':request.POST.get('username')})
             else:
